7195280756b62121a6ecae6a6f140b1c3be94557
Donatas Domres <donatas.domres@gmail.com>
2022-08-11 15:23:01 +0300

    ss

--- MouseTagProject/Models/AddCandidateDto.cs
+++ MouseTagProject/Models/AddCandidateDto.cs
@@ -2,13 +2,13 @@
 {
     public class AddCandidateDto
     {
-        public DateTime WhenWasContacted { get; set; }
+        public DateTime WhenWasContacted { get; set; } = DateTime.MinValue;
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Linkedin { get; set; }
         public string Comment { get; set; } = "";
         public bool Available { get; set; }
-        public List<int> TechnologyIds { get; set; }
-        public DateTime WillBeContacted { get; set; }
+        public List<int> TechnologyIds { get; set; } = new List<int>();
+        public DateTime WillBeContacted { get; set; } = DateTime.MinValue;
     }
 }
