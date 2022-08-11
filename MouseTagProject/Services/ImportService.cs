3b1d7aaa318691d310441d7e43fd4fe63ddde736
Donatas Domres <donatas.domres@gmail.com>
2022-08-11 09:54:05 +0300

    Added Importfromexel

---
+++ MouseTagProject/Services/ImportService.cs
@@ -0,0 +1,54 @@
+﻿using MouseTagProject.Context;
+using MouseTagProject.DTOs;
+using MouseTagProject.Models;
+
+namespace MouseTagProject.Services
+{
+    public class ImportService
+    {
+        public List<Candidate> ImportFromExel(List<ImportCandidateDto> importCandidates)
+        {
+            List<Candidate> candidates = new List<Candidate>();
+
+            foreach (var importCandidate in importCandidates)
+            {
+                Candidate candidate = new Candidate();
+
+                List<DateTime> dateTime = new List<DateTime>();
+
+                if (importCandidate.DateListAsInt.GetType() == typeof(System.Int32))
+                {
+                    var dateInt = Convert.ToInt32(importCandidate.DateListAsInt);
+                    DateTime dt = DateTime.FromOADate(dateInt);
+                    dateTime = new List<DateTime>() { dt };
+                }
+                if (importCandidate.DateListAsInt.GetType() == typeof(System.String))
+                {
+                    var splitedDate = importCandidate.DateListAsInt.Split(new char[] { ' ', ',', '.' }).ToList();
+
+                    var dateInt = 0;
+                    splitedDate.ForEach(date =>
+                    {
+                        dateInt = Convert.ToInt32(date);
+                        DateTime dt = DateTime.FromOADate(dateInt);
+                        var dateTime = new List<DateTime>() { dt };
+                    });
+                }
+
+                candidate.Name = importCandidate.Name;
+                candidate.Surname = importCandidate.Surname;
+                candidate.Linkedin = importCandidate.Linkedin;
+                candidate.Comment = importCandidate.Comment;
+                candidate.WhenWasContacted
+                candidate.Available = importCandidate.Open;
+
+                var technologies = importCandidate.TechnologyListAsString.Split(new char[] { ' ', ',' });
+                IEnumerable<string> strings = technologies;
+                //  candidate.Technologies = 
+                //candidate.WhenWasContacted = 
+                // candidates.Add(candidate);
+            }
+            return candidates;
+        }
+    }
+}
